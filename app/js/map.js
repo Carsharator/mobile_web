7c5ebdce613d177c7bab4f8c51612d4d381bb71c
broderzon <broderzon@gmail.com>
2016-06-18 15:44:03 +0300

    map

--- app/js/map.js
+++ app/js/map.js
@@ -1,10 +1,57 @@
 ;function initMap() {
     // Create a map object and specify the DOM element for display.
+
+    var mMy = new google.maps.Marker(), mCars = [], mPos = new google.maps.Marker();
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 55.760034, lng: 37.66464699999999},
         scrollwheel: false,
         zoom: 17
     });
+    var searchAddress = document.getElementById('searchAddress');
+
+    var types = document.getElementById('type-selector');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchAddress);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+    var autocomplete = new google.maps.places.Autocomplete(searchAddress);
+    autocomplete.bindTo('bounds', map);
+
+    autocomplete.addListener('searchAddress', function() {
+        mPos.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        mPos.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+        mPos.setPosition(place.geometry.location);
+        mPos.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+    });
+
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -16,7 +63,8 @@
             //infoWindow.setPosition(pos);
             //infoWindow.setContent('Location found.');
             map.setCenter(pos);
-            console.debug(pos)
+            mMy.setPosition(pos);
+            mMy.setMap(map);
         }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
         });
