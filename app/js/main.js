942fd74445ebbf7e9dacdb0ea6c380505c2ab9f8
aleksandrrodin <hawk2889@gmail.com>
2016-06-18 19:51:58 +0300

    fix camera and registration and authorization

--- app/js/main.js
+++ app/js/main.js
@@ -3,37 +3,39 @@ $(document).ready(function() {
     $('.button-collapse').sideNav();
 
 
-    window.addEventListener("DOMContentLoaded", function() {
-        // Grab elements, create settings, etc.
-        var video = document.getElementById("video"),
-            videoObj = { "video": true },
-            errBack = function(error) {
-                console.log("Video capture error: ", error.code);
-            };
-
-        // Put video listeners into place
-        if(navigator.getUserMedia) { // Standard
-            navigator.getUserMedia(videoObj, function(stream) {
-                video.src = stream;
-                video.play();
-            }, errBack);
-        } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-            navigator.webkitGetUserMedia(videoObj, function(stream){
-                video.src = window.webkitURL.createObjectURL(stream);
-                video.play();
-            }, errBack);
-        }
-        else if(navigator.mozGetUserMedia) { // Firefox-prefixed
-            navigator.mozGetUserMedia(videoObj, function(stream){
-                video.src = window.URL.createObjectURL(stream);
-                video.play();
-            }, errBack);
-        }
-    }, false);
 
 
 
-    if (document.getElementsByClassName('wrap_camera')) {
+
+    if (document.getElementsByClassName('wrap_camera').length) {
+
+        window.addEventListener("DOMContentLoaded", function() {
+            // Grab elements, create settings, etc.
+            var video = document.getElementById("video"),
+                videoObj = { "video": true },
+                errBack = function(error) {
+                    console.log("Video capture error: ", error.code);
+                };
+
+            // Put video listeners into place
+            if(navigator.getUserMedia) { // Standard
+                navigator.getUserMedia(videoObj, function(stream) {
+                    video.src = stream;
+                    video.play();
+                }, errBack);
+            } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+                navigator.webkitGetUserMedia(videoObj, function(stream){
+                    video.src = window.webkitURL.createObjectURL(stream);
+                    video.play();
+                }, errBack);
+            }
+            else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+                navigator.mozGetUserMedia(videoObj, function(stream){
+                    video.src = window.URL.createObjectURL(stream);
+                    video.play();
+                }, errBack);
+            }
+        }, false);
 
         var widthCamBlock = document.getElementsByClassName('wrap_camera')[0].clientWidth;
         var heightCamBlock = widthCamBlock - (widthCamBlock / 3)
