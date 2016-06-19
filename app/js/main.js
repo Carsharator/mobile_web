94e850d73233725d2fd545752d77fcbfee5ac618
aleksandrrodin <hawk2889@gmail.com>
2016-06-19 11:22:56 +0300

    lk

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,43 @@ $(document).ready(function() {
     $('.button-collapse').sideNav();
 
 
-    $( ".btn-large" ).on( "click", function() {
+    $( "#authorization .btn-authorization" ).on( "click", function() {
+        var login_name = $('#login_name').val();
+        var password = $('#password').val();
+
+
+        var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+
+        var xhr = new XHR();
+        var data3 =  "license_number=" + login_name;
+        xhr.open('GET', 'https://46.101.141.101:3001/users/?' + data3, true);
+
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        // xhr.setRequestHeader('Origin', 'https://localhost:4004/views/registration.html');
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState != 4) return;
+
+
+            localStorage.setItem("USER", this.responseText);
+            window.location.href = 'https://localhost:4004/views/lk.html';
+
+        };
+
+
+        xhr.onerror = function() {
+            console.log( 'Ошибка ' + this.status );
+        };
+
+
+
+        xhr.send();
+
+       
+    });
+
+
+    $( "#createNewUser .btn-createNewUser" ).on( "click", function() {
 
 
         var first_name = $('#first_name').val();
@@ -16,69 +52,57 @@ $(document).ready(function() {
         var driver_number = $('#driver_number').val();
         var driver_number_time = $('#driver_number_time').val();
 
-        var data = {first_name: first_name,
-                    last_name: sur_name,
-                    patronymic_name: patronymic_name,
-                    login: login_name,
-                    password: password,
-                    phone: phone,
-                    email: email,
-                    license_number: driver_number,
-                    license_valid_until: driver_number_time};
+        // var data = {first_name: first_name,
+        //             last_name: sur_name,
+        //             patronymic_name: patronymic_name,
+        //             login: login_name,
+        //             password: password,
+        //             phone: phone,
+        //             email: email,
+        //             license_number: driver_number,
+        //             license_valid_until: driver_number_time};
 
-        var dataToJson = JSON.stringify(data);
+        // var dataToJson = JSON.stringify(data);
 
-        console.log(data)
-        console.log(dataToJson);
-
-
-        // function jsonCallback2(data) {
-        //
-        // }
-        //
-        // $.ajax({
-        //     type: "POST",
-        //     url: "https://46.101.141.101:3001/users/new",
-        //     jsonCallback: jsonCallback2,
-        //     contentType: "application/json",
-        //     async: false,
-        //     dataType: 'jsonp',
-        //     success: function(dataRes){
-        //         console.log(dataRes);
-        //     },
-        //     error: function(){
-        //         console.log('error')
-        //     }
-        //
-        // });
-// (1)
-
-
-        var formData = new FormData(document.forms.newUser);
         var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 
         var xhr = new XHR();
 
-// (2) запрос на другой домен :)
         xhr.open('POST', 'https://46.101.141.101:3001/users/new', true);
 
-        // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
         // xhr.setRequestHeader('Origin', 'https://localhost:4004/views/registration.html');
-
 
         xhr.onreadystatechange = function() {
             if (this.readyState != 4) return;
 
-            alert( this.responseText );
-        }
+            console.log( this.responseText );
+        };
 
 
         xhr.onerror = function() {
-            alert( 'Ошибка ' + this.status );
-        }
+            console.log( 'Ошибка ' + this.status );
+        };
+        console.log(dataToJson);
 
-        xhr.send(dataToJson);
+        var data2 = "first_name=" +  first_name + "&last_name=" + sur_name + "&patronymic_name=" + patronymic_name + "&license_number=" + driver_number + "&login=" +login_name+ "&password=" +password+ "&phone=" +phone+ "&email" +email+ "&license_valid_until=" + driver_number_time;
+        xhr.send(data2);
+
+
     });
+
+
+    if(window.location.href.search('lk.html') != -1 ) {
+        var objStorage = JSON.parse(localStorage.USER);
+        console.log(objStorage)
+
+
+        $('.user i').next().text(objStorage.user.login)
+
+    }
+
+
+
 
 
     if (document.getElementsByClassName('wrap_camera').length) {
